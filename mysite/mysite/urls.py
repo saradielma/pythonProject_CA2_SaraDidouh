24c92c9100ed2c9eb761756e489a4d1c9526f2e4
SaraIes <saradidouh@iesmontsia.org>
2022-12-07 13:15:17 +0000

    signup

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -6,6 +6,7 @@ from django.views.generic.base import TemplateView
 urlpatterns = [
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
+    path("accounts/", include("accounts.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
     path("", TemplateView.as_view(template_name="home.html"), name="home"),
 ]
