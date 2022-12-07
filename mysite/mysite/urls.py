8dca1ee8d724d45ad2e7d0f96f949ccf0166d7d9
SaraIes <saradidouh@iesmontsia.org>
2022-12-07 12:42:28 +0000

    base and home page

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import TemplateView
 
 """Assign the urls to each view"""
 urlpatterns = [
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
